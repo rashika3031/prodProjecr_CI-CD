b7d73cc29bb915bfb285c8acbe172f8bcab0ce50
rashika3031 <rashika.sahu@nagarro.com>
2023-09-29 10:02:58 +0530

    adding workflow file

---
+++ src/main/java/com/prodProject/service/AllService.java
@@ -0,0 +1,18 @@
+package com.prodProject.service;
+
+public class AllService {
+	
+	public float add(float i,float j) {
+		return i+j;
+	}
+	public float sub(float i,float j) {
+		return i-j;
+	}
+	public float mul(float i,float j) {
+		return i*j;
+	}
+	public float div(float i,float j) {
+		return i/j;
+	}
+
+}
---
+++ src/test/java/com/prodProject/OperationControllerTest.java
@@ -0,0 +1,37 @@
+package com.prodProject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+
+import com.prodProject.service.AllService; 
+
+public class OperationControllerTest {
+	AllService addctrl=new AllService();
+	@Test
+	void testAdd() {
+		
+		float actual=addctrl.add(10,10);
+		float expected=20;
+		assertEquals(expected,actual);
+	}
+	@Test
+	void testSub() {
+		float actual=addctrl.sub(20,10);
+		float expected=10;
+		assertEquals(expected,actual);
+	}
+	@Test
+	void testMul() {
+		float actual=addctrl.mul(5,5);
+		float expected=25;
+		assertEquals(expected,actual);
+	}
+	@Test
+	void testDiv() {
+		float actual=addctrl.div(100,10);
+		float expected=10;
+		assertEquals(expected,actual);
+	}
+
+}
